1314dc24331035770d8ab72a82313135e0495a69
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-04-21 15:39:42 -0300

    Sistema de Sale Options finalizado com sucesso

---
+++ GerenciadorProdutos/Entities/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorProdutos.Entities {
+    class Sale {
+        public int Id { get; private set; }
+        public Product Product { get; private set; }
+        public int Quantity { get; private set; }
+        public DateTime SaleDate { get; private set; }
+        public Sale(int id, Product product, int quantity, DateTime saleDate) {
+            Id = id;
+            Product = product;
+            Quantity = quantity;
+            SaleDate = saleDate;
+        }
+    }
+}
---
+++ GerenciadorProdutos/Service/SaleRecorder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GerenciadorProdutos.Entities;
+
+namespace GerenciadorProdutos.Service {
+    class SaleRecorder {
+        public List<Sale> Sales { get; private set; }
+
+        public SaleRecorder() {
+            Sales = new List<Sale>();
+        }
+
+        public void RegisterSale(Product product, int Quantity) {
+            Sales.Add(new Sale(Sales.Count, product, Quantity, DateTime.Now));
+            
+        }
+
+        public void ListSales() {
+            Console.WriteLine("Sales Records:");
+            if(Sales.Count == 0) {
+                Console.WriteLine("No sales registered.");
+                return;
+            }
+            foreach (var sale in Sales) {
+                Console.WriteLine($"Sale ID: {sale.Id}, Product: {sale.Product.Name}, Quantity: {sale.Quantity}, Date: {sale.SaleDate}");
+            }
+        }
+
+        public void DeleteSale(Sale Sale) {
+            Sales.Remove(Sale);
+
+        }
+
+        public Sale GetLastSale() {
+            if (Sales.Count == 0) {
+                return null;
+            }
+            return Sales.Last();
+        }
+    }
+}
